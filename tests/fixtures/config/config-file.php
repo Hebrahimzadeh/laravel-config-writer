0e22de019ee1bf5e540ed1fd46fbe6c884b61d61
Hossein <abi.hossein@gmail.com>
2021-08-08 12:38:06 +0430

    chore: add fixture config file

--- tests/fixtures/config/config-file.php
+++ tests/fixtures/config/config-file.php
@@ -1,13 +1,11 @@
 <?php
 return [
-    'test_a' => 'string',
-    'test_two' => 0000,
+    'test_a' => 'this is change test_a.',
+    'test_two' => 121,
     'test_array_one' => [
-        'index_1' => 000,
-        'index_2' => 'string',
-        'index_3' => [
-            1,2,3
-        ],
-        'index_4' => 'https://string.com'
+        'index_1' => 'string index instead int',
+        'index_2' => 222,
+        'index_3' => 'string index instead array',
+        'index_4' => [1]
     ]
 ];
